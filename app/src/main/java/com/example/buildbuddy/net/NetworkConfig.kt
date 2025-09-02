b3aa22d99604171a7c4d2f5d9d6931dc62d481af
Abhishek Roushan <147068739+Abhi95081@users.noreply.github.com>
2025-09-03 00:02:39 +0530

    NetworkConfig commit

---
+++ app/src/main/java/com/example/buildbuddy/net/NetworkConfig.kt
@@ -0,0 +1,6 @@
+package com.example.buildbuddy.net
+
+object NetworkConfig {
+    // ❗ change this to your server (e.g., http://192.168.1.10:8000/)
+    const val BASE_URL = "http://10.0.2.2:8000/" // Android emulator -> host machine
+}
